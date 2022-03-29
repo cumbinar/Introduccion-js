23eb55c8e25ccf50612b2dae9288541ef5cddc28
cumbinar <cumbiweb@gmail.com>
2022-03-29 09:32:17 -0500

    inicio curso de js

---
+++ js/app.js
@@ -0,0 +1,17 @@
+/*alert('Hola Mundo');
+const nombre = prompt('¿cuál es tu nombre?');
+document.querySelector('.contenido').innerHTML = `${nombre} , Está aprendiendo js Moderno`;*/
+const precio = 108800;
+const producto = "Monitor de 24 pulgadas";
+console.log("El " + producto + " es de " + precio + " pesos");
+console.log(`El artefacto ${producto} tiene un precio de $ ${precio}`);
+
+const refran = "      camaron que se duerme se ahoga        ";
+console.log(refran);
+console.log(refran.trimStart().trimEnd());
+console.log(refran.trim());
+
+console.log(producto);
+console.log(producto.replace('pulgadas', '"'));
+console.log(producto.replace('Monitor', 'Pantalla curva'));
+
